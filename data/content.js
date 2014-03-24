391e5ebfb753d42e69c25ab101f1516d2281533a
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-03-25 02:23:25 +0400

    Копируем результат в буфер обмена и закрываем окно

--- data/content.js
+++ data/content.js
@@ -3,9 +3,12 @@
 self.port.on('show', function(args) {
     var input = document.getElementById("source");
     var output = document.getElementsByClassName("output")[0];
+    var closeHandler = document.getElementsByClassName('closer')[0];
     var copyHandler = document.getElementById('copy');
     var cyrillicLetters = " 1234567890-=!\"№;%:?*()_+йцукенгшщзхъфывапролджэ\/ячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/|ЯЧСМИТЬБЮ,";
     var latinLetters = " 1234567890-=!@#$%^&*()_+qwertyuiop[]asdfghjkl;'\<zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:\"|>ZXCVBNM<>?";
+
+    input.focus();
 
     input.addEventListener("blur", function (evt) {
         var inputStr = input.value;
@@ -31,4 +34,13 @@ self.port.on('show', function(args) {
 
         copyHandler.className = '';
     }, false);
+
+    closeHandler.addEventListener('click', function() {
+        self.port.emit('panel-close');
+    });
+
+    copyHandler.addEventListener('click', function(event) {
+        self.port.emit('panel-copy', output.innerHTML);
+        self.port.emit('panel-close');
+    });
 });
